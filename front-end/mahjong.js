56b81c00bb60680ac27ff75c25e8cf80ded51cc6
Loop1397 <shwlsdn0906@gmail.com>
2024-05-06 17:45:03 +0900

    🎨Feat : 모든 마작패에 대한 몸통/머리 검사 알고리즘 구현
    기존 만수패에만 대응하던 몸통/머리 검사 알고리즘을 수패/자패 전부가 포함된 패에도 검사 할 수 있도록 개선.

--- front-end/mahjong.js
+++ front-end/mahjong.js
@@ -1,25 +1,24 @@
-/*
-마작 패 검사하는 알고리즘을 만들거임.
-마작은 모든 패가 4개씩 존재함.
-마작은 게임을 이기기 위해선 14개의 패로 머리1개와 몸통 4개를 만들어여야함
-머리는 같은 패 2개가 있으면 되고, 몸통은 같은패 3개나, 연속되는 패 3개가 있으면 충족됨.
-*/
-
-/** 
+/**
  * TODO
- * [ ] : 그리드법으로 바꿔야함
+ * [ ] : 국사무쌍 판정 제작
  */
+
+/**
+ * 
+ * @param {*} handTileArr 
+ * @param {*} completedTileArr 
+ * @param {*} cntT 
+ * @param {*} cntO 
+ * @param {*} cntH 
+ */
+
 // 인자로 몸통이나 머리 등을 뗀 손패 배열, 떼어진 몸통과 머리가 들어간 완성패 배열, 이 들어가야함
 // 처음 전달받는 건 손패 배열과 빈 배열
-const mahjong = (handTileArr, completedTileArr, cntO, cntT) => {
-    result = [];
-    // 먼저 몸통을 뗌
+const mahjong = (handTileArr, completedTileArr, cntT, cntO, cntH) => {
     // 만약 조건이 맞아서 몸통이 떼어질 때마다 처음부터 다시 검사 < 재귀함수
-    // 완성 판단? -> 최종적으로 handTileArr.length = 0이되면 완성
-    // 국사무쌍은 따로 제작
 
-    if (handTileArr.length === 0 && (completedTileArr.length === 5 || completedTileArr.length === 7)){
-        result.push(completedTileArr, cntT, cntO);
+    if ((cntT + cntO === 4 && cntH === 1) || cntH === 7){
+        result.push(completedTileArr, cntT, cntO, cntH);
     }
 
     // 커쯔(3개의 같은 패로 이루어진 몸통) 판별
@@ -30,13 +29,15 @@ const mahjong = (handTileArr, completedTileArr, cntO, cntT) => {
         cntT += 1;
         newCompletedTileArr.push(newHandTileArr.splice(0, 3));
         
-        mahjong(newHandTileArr, newCompletedTileArr, cntT, cntO);
+        mahjong(newHandTileArr, newCompletedTileArr, cntT, cntO, cntH);
+        cntT -= 1;
     }
 
     // 슌쯔(3개의 연속된 패로 이루어진 몸통) 판별
     // 만약 현재 위치 기준 +1 및 +2의 값이 발견되지 않는 경우, location[1]과 location[2]는 -1이 됨(location[0]은 무조건 현재 숫자)
+    // 자패는 슌쯔가 적용되면 안되기 때문에 handTileArr[0]도 체크
     let location = [0, handTileArr.findIndex(e => e === (handTileArr[0] + 1)), handTileArr.findIndex(e => e === (handTileArr[0] + 2))];
-    if(location[1] !== -1 && location[2] !== -1) {
+    if(location[1] !== -1 && location[2] !== -1 && handTileArr[0] < 30) {
         let newHandTileArr = [...handTileArr];
         let newCompletedTileArr = [...completedTileArr];
 
@@ -50,39 +51,51 @@ const mahjong = (handTileArr, completedTileArr, cntO, cntT) => {
             newHandTileArr.splice(e, 1);
         });
         
-        mahjong(newHandTileArr, newCompletedTileArr, cntT, cntO);
+        mahjong(newHandTileArr, newCompletedTileArr, cntT, cntO, cntH);
+        cntO -= 1;
     }
-
 
     // 머리 판별
     if(handTileArr[0] === handTileArr[0 + 1] && handTileArr.length !== 0) {
         let newHandTileArr = [...handTileArr];
         let newCompletedTileArr = [...completedTileArr];
+
+        cntH += 1;
         newCompletedTileArr.push(newHandTileArr.splice(0, 2));
 
-        mahjong(newHandTileArr, newCompletedTileArr, cntT, cntO);
+        mahjong(newHandTileArr, newCompletedTileArr, cntT, cntO, cntH);
     }
 }
 
 
 
 let result = [];
-//커쯔 카운트
-cntT = 0;
-//슌쯔 카운트
-cntO = 0;
 const arr = [
-    [1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 7, 7],
+    // [1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 7, 7, 7, 8],
     [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7],
-    [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9],
-    [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5],
-    [1, 1, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9, 9],
-    [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5]
+    // [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9],
+    // [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5],
+    // [1, 1, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9, 9],
+    // [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5],
+    // [1, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 7],
+    // [1, 1, 1, 2, 2, 3, 3, 4, 21, 22, 23, 21, 22, 23],
+    // [31, 31, 31, 32, 32, 32, 33, 33, 33, 34, 34, 34, 35, 35]
 ];
 
 arr.forEach(element => {
+    result = [];
+
+    handCount = new Array(38).fill(0);
+    element.forEach(e => {
+        handCount[e] += 1;
+    });
+
+    // console.log(`handTile : ${handCount}`);
+    // handCheck(handCount, [], 0, 0, 0);
+    // console.log(result);
+
     console.log(`handTile : ${element}`);
-    mahjong(element, [], cntT, cntO);
+    mahjong(element, [], 0, 0, 0);
     console.log(result);
 });
 
